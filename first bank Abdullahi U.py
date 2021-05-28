ce425b49d6ccf8b274b234db3847f668b8fd885f
abdul611 <81785454+abdul611@users.noreply.github.com>
2021-05-28 16:17:30 +0100

    Add files via upload

---
+++ first bank Abdullahi U.py
@@ -0,0 +1,86 @@
+USSD_CODE = ("FIRST BANK : *894#")
+print(USSD_CODE)
+display = "Please type in your banks ussd code: 894 "
+print(display)
+display_0= "You have entered the code for FirstBank of Nigeria"
+print(display_0)
+list = [ "1.Check your balance",  "2.Transfer",  "3.Airtime", "4.Transaction history"]
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+option_1= 'Please select an option: 1'
+print(option_1)
+display_1= 'Your Acct 314XXXX556 On 25-MAY-2021 20:30:41 Bal: NGN589.81CR'
+print (display_1)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05)
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+option_2 = 'Please select an option: 2'
+print (option_2)
+display_7= 'Enter account number you want to transfer money to: 3147308556 '
+print(display_7)
+display_6='Enter Amount : 4000'
+print(display_6)
+display_3= "Enter pin :4239"
+print(display_3)
+display_4= "Do you want to send 4000 to 3147308556?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05)
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+option_3= "Please select an option: 3"
+print(option_3)
+list_1=[ "1.For self" , "2.For others" ]
+print( list_1[0])
+print ( list_1[1])
+display_4 = "Enter 1 or 2 : 2"
+print(display_4)
+display_6= 'Enter Amount : 4000'
+print(display_6)
+display_7= 'Enter  number you want to buy airtime for: 07011823228'
+print(display_7)
+display_4= "Do you want to buy 4000 naira worth of airtime for 07011823228?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05) 
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+option_4= "Please select an option: 4"
+print(option_4)
+display_11="An SMS message will be sent to you shortly."
+print(display_11)
